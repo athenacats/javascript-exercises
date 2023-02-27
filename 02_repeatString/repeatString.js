d75afe426cd4f772e0a76e316cb3dc2ea4c219cd
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-27 18:17:16 +0300

    Add explanatory notes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
 const repeatString = function(str, num) {
+//By my understanding, you declare a blank string ''. Its empty because
+//the conditions that you input in the curly brackets will determine what the
+//outcome is. num is going to be the number, so it can be compared with i
+//+= adds the item to the left to that on the right;
+//ie string = string + str. so if str is hey;
+// if i = 0,  string = '' + 'hey
+// i = 1      string = 'hey' + 'hey'
+// i =        string = 'heyhey' + 'hey' etc
     let string = '';
     if (num < 0) {
         return 'ERROR';
