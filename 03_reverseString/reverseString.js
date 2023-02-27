40dfc3d6dd8968328ce02ac180041a824877bea9
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-27 19:41:01 +0300

    Solve reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split(""); {
+        reversal = array.reverse()
+    }
+    return reversal.join('');
 };
 
 // Do not edit below this line
