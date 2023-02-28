5ed4a645812de8ad967c721fccba0f1ff834332d
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-28 11:10:44 +0300

    Solve 05 sumAll
    succesful without much error

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || !Number.isFinite(a) || !Number.isFinite(b)) {
+        return 'ERROR';
+    }
+    if (b > a) {
+        for (i = a; i <= b; i++) {
+            sum += i;
+        } 
+        return sum;
+    } else {
+        for (i = b; i <= a; i++) {
+            sum += i;
+        }
+        return sum;
+    } 
+   
+}
 
-};
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
