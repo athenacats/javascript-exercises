83787197644890751e1522906f7a72a7b700fb33
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-28 07:07:26 +0300

    Solve removeFromArray
    combined loops as well as indexOf. Initially had option that used push but would require a loop in a loop. opted for the indexOf-splice instead

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,39 @@
-const removeFromArray = function() {
+function removeFromArray() {
+    const myArray = arguments[0];
+    for ( let i = 1; i < arguments.length; i++) {
+        const index = (myArray.indexOf(arguments[i]));
+        if (index > -1) {
+            myArray.splice(index, 1);
+        }
+    }
+    return myArray;
+}
 
-};
+
+// check https://www.programiz.com/javascript/examples/remove-array-item for clear examples
+
+//const removeFromArray = function(array, position) {
+    //const index = array.indexOf(position);
+    // if index is in the array, remove it
+    //remember index starts at 0, so that means in that number is in the
+    //array, its index is going to be > -1
+    //if (index > -1) {
+        //array.splice(index, 1);
+    //}
+    //return array;
+//};
+
+//other method
+//function removeFromArray(array, position) {
+    //const newArray = [];
+
+    //for ( let i = 0; i < array.length; i++) {
+        //if(array[i] !== position) {
+            //newArray.push(array[i]);
+        //}
+    //}
+    //return newArray;
+//}
 
 // Do not edit below this line
 module.exports = removeFromArray;
