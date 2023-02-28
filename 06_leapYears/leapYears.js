99bacd3c4fc665451fca38cee569eb1b5ce429c0
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-28 12:11:16 +0300

    Solve 06 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+//the order of code matters!!
+//if you dont start with the century function and start with the % 4 instead,
+//it will return non-leap century years as true
 
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 != 0 ){
+        return false;
+    } if (year % 4 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
