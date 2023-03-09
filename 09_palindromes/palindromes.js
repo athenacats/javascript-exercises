0505aaa0208ce3bb3952338dacadaf10922fe96d
athenacats <124017569+athenacats@users.noreply.github.com>
2023-03-09 10:11:46 +0300

    Complete palindromes and fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    return str == str.split("").reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+/*const splitStr = string.toLowerCase().replace(/[^a-z]/g, "").split('');
+    const revStr = splitStr.reverse();
+    const joinStr = revStr.join('');
+    if (string != joinStr) {
+        return true;
+    }
+    else {return false;}*/
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (typeof number === "string") {
+        number = parseInt(number)
+    }
+    if (number < 0) {
+        return'OOPS';
+    }
+    let sum = 0;
+    let a = 0;
+    let b = 1;
+    for (i = 1; i < number; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    return b;
 };
 
 // Do not edit below this line
